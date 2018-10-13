5974d37c2a98b162f49ee1088d5242f681a2da79
dl541 <dl541@cam.ac.uk>
2018-10-13 16:58:26 +0100

    First commit of project

---
+++ Assets/MovingCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour {
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateSpeed();
+        gameObject.transform.position += new Vector3(0f, 0f, speed);
+    }
+    // Update the speed of the cube
+    void UpdateSpeed()
+    {
+        if (gameObject.transform.position.z < -15.0f) speed = 0.1f;
+        else if (gameObject.transform.position.z > 15.0f) speed = -0.1f;
+    }
+}
