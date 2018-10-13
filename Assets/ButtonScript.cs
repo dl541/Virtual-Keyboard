1321ef1ac1ffc5a90ef51b34fe9bf8e6cf9e3d43
dl541 <dl541@cam.ac.uk>
2018-10-13 17:07:24 +0100

    First commit to github

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On Trigger Enter is pressed");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("On Trigger Stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+    }
+}
