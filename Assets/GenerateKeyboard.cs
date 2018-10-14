078b3492dab20f2bee79692e648ebc53e31959a0
dl541 <dl541@cam.ac.uk>
2018-10-14 17:10:19 +0100

    Add script to generate second button

--- Assets/GenerateKeyboard.cs
+++ Assets/GenerateKeyboard.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GenerateKeyboard : MonoBehaviour {
+    public GameObject keyboardBase;
+    public GameObject buttonPrefab;
 
 	// Use this for initialization
 	void Start () {
-		
+        GenerateKeys();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void GenerateKeys()
+    {
+        instantiateKey();
+    }
+
+    void instantiateKey()
+    {
+        GameObject newButton = Instantiate(buttonPrefab) as GameObject;
+        newButton.transform.SetParent(keyboardBase.transform, false);
+        newButton.transform.position = new Vector3(50f, 50f, 50f);
+        GameObject text = newButton.transform.Find("TextMeshPro Text").gameObject;
+        TextMeshProUGUI textMesh = text.GetComponent<TextMeshProUGUI>();
+        textMesh.SetText("W");
+    }
 }
