916a336da6dc3806f366ae1afc26e949e21421d6
dl541 <dl541@cam.ac.uk>
2018-11-04 17:06:55 +0000

    Add C# KeyboardPressingLoggerClient

---
+++ Assets/KeyboardPressingLoggerClient.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public class KeyboardPressingLoggerClient : MonoBehaviour {
+
+    #region private members 	
+    private TcpClient socketConnection;
+    private Thread clientReceiveThread;
+    #endregion
+    // Use this for initialization 	
+    void Start()
+    {
+        ConnectToTcpServer();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    /// <summary> 	
+    /// Setup socket connection. 	
+    /// </summary> 	
+    private void ConnectToTcpServer()
+    {
+        try
+        {
+            clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+            clientReceiveThread.IsBackground = true;
+            clientReceiveThread.Start();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("On client connect exception " + e);
+        }
+    }
+    /// <summary> 	
+    /// Runs in background clientReceiveThread; Listens for incomming data. 	
+    /// </summary>     
+    private void ListenForData()
+    {
+        try
+        {
+            socketConnection = new TcpClient("localhost", 38300);
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                // Get a stream object for reading 				
+                using (NetworkStream stream = socketConnection.GetStream())
+                {
+                    int length;
+                    // Read incomming stream into byte arrary. 					
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        var incommingData = new byte[length];
+                        Array.Copy(bytes, 0, incommingData, 0, length);
+                        // Convert byte array to string message. 						
+                        string serverMessage = Encoding.ASCII.GetString(incommingData);
+                        Debug.Log("server message received as: " + serverMessage);
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+}
