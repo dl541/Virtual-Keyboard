4e98e76769f44d7d914e2d1ab7e85b3ab77340fc
dl541 <dl541@cam.ac.uk>
2018-11-05 23:59:23 +0000

    Add scripts for Balloon feedback and Spring feedback

---
+++ Assets/BalloonAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BalloonAnimation : ButtonAnimation {
+
+    override public void pressAnimation(Button button)
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(button.gameObject, pointer, ExecuteEvents.pointerEnterHandler);
+        ExecuteEvents.Execute(button.gameObject, pointer, ExecuteEvents.pointerDownHandler);
+    }
+
+    override public void releaseAnimation(Button button)
+    {
+        var pointer = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(button.gameObject, pointer, ExecuteEvents.pointerUpHandler);
+    }
+}
---
+++ Assets/SpringAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpringAnimation : ButtonAnimation{
+
+    public static float maxDepth = 10f;
+
+    override public void pressAnimation(Button button)
+    {
+        button.gameObject.transform.position = new Vector3(button.gameObject.transform.position.x, button.gameObject.transform.position.y, maxDepth);
+    }
+
+    override public void releaseAnimation(Button button)
+    {
+        button.gameObject.transform.position = new Vector3(button.gameObject.transform.position.x, button.gameObject.transform.position.y, 0f);
+    }
+}
