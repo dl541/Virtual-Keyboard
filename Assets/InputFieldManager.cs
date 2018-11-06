ae56ab85f7544e89af7842d491b2c0bd011e41d8
dl541 <dl541@cam.ac.uk>
2018-11-06 00:30:07 +0000

    Add character limit to input field

--- Assets/InputFieldManager.cs
+++ Assets/InputFieldManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class InputFieldManager : MonoBehaviour {
 
     private static InputField inputField;
+    private int characterLimit = 30;
 
 	// Use this for initialization
 	void Start () {
-        inputField = gameObject.GetComponent<InputField>();	
+        inputField = gameObject.GetComponent<InputField>();
+        inputField.characterLimit = characterLimit;
 	}
 	
 	// Update is called once per frame
@@ -19,6 +21,10 @@ public class InputFieldManager : MonoBehaviour {
 
     public void append(string character)
     {
+        if (inputField.characterLimit == inputField.text.Length)
+        {
+            inputField.text = inputField.text.Substring(1, inputField.text.Length - 1);
+        }
         inputField.text += character;
     }
 }
