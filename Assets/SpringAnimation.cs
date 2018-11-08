e51225c471607735f645c210fa0c2b6c1260f784
dl541 <dl541@cam.ac.uk>
2018-11-08 06:42:22 +0000

    Remove rigidbody component from spring keyboard

--- Assets/SpringAnimation.cs
+++ Assets/SpringAnimation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SpringAnimation : ButtonAnimation{
@@ -9,11 +10,25 @@ public class SpringAnimation : ButtonAnimation{
 
     override public void pressAnimation(Button button)
     {
+        //Move keyboard button
         button.gameObject.transform.position = new Vector3(button.gameObject.transform.position.x, button.gameObject.transform.position.y, maxDepth);
+
+        //Shading
+        var pointer = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(button.gameObject, pointer, ExecuteEvents.pointerEnterHandler);
+        ExecuteEvents.Execute(button.gameObject, pointer, ExecuteEvents.pointerDownHandler);
+
+        //Play audio
+        playAudio();
+
     }
 
     override public void releaseAnimation(Button button)
     {
         button.gameObject.transform.position = new Vector3(button.gameObject.transform.position.x, button.gameObject.transform.position.y, 0f);
+
+        var pointer = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(button.gameObject, pointer, ExecuteEvents.pointerUpHandler);
+
     }
 }
