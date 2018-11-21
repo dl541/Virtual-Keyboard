1288d08c19729c57405e93cab9076614a1024870
dl541 <dl541@cam.ac.uk>
2018-11-21 18:03:25 +0000

    Add script to attach text to custom buttons

---
+++ Assets/AddText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AddText : MonoBehaviour {
+
+    public GameObject buttonTextPrefab;
+    private GameObject buttonText;
+    private bool textAttach = true;
+
+	// Use this for initialization
+	void Start () {
+        buttonText = Instantiate(buttonTextPrefab) as GameObject;
+	}
+    
+    // Text attached after the object is instantiated to avoid text material not found issue
+    void attachText()
+    {
+        buttonText.transform.SetParent(gameObject.transform, false);
+        buttonText.transform.localPosition = new Vector3(0f, 0f, -0.01f);
+        buttonText.transform.localRotation = Quaternion.identity;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (textAttach)
+        {
+            attachText();
+            textAttach = false;
+        }
+
+    }
+}
