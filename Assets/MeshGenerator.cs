54045e0ea0f29446a2117b99b8a131a30c5ce4ba
dl541 <dl541@cam.ac.uk>
2018-11-21 18:03:57 +0000

    Add script to generate mesh for custom buttons

---
+++ Assets/MeshGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour {
+
+    float width = 100;
+    float height = 100;
+
+	// Use this for initialization
+	void Start () {
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[4];
+
+        vertices[0] = new Vector3(-width, height);
+        vertices[1] = new Vector3(width, height);
+        vertices[2] = new Vector3(width, -height);
+        vertices[3] = new Vector3(-width, -height);
+        mesh.vertices = vertices;
+        mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+        gameObject.GetComponent<MeshFilter>().mesh = mesh; 
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
