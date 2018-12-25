1c702bf381c04172ba659ebb1976d977ffa43c0f
dl541 <dl541@cam.ac.uk>
2018-12-25 22:17:53 +0800

    Add missing files

---
+++ Assets/UserPointerManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserPointerManager : MonoBehaviour {
+
+    private double mass = 100;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 prevPosition;
+	// Use this for initialization
+	void Start () {
+        prevPosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
