a2aa9cb903f88ca87b6e0a489b932ea2be74f0ed
unknown <dl541@cam.ac.uk>
2019-03-13 16:18:42 +0000

    Write logs into buffer first before printing in a file

--- Assets/PrintMarkerLocations.cs
+++ Assets/PrintMarkerLocations.cs
@@ -13,13 +13,15 @@ public class PrintMarkerLocations : MonoBehaviour
     private StreamWriter sw;
     public OptitrackStreamingClient streamingClient;
     private int logIndex = 0;
-    private string line_;
     public GameObject boundingBox;
+    public int logBufferSize = 2500000;
+    private string[] logBuffer;
 
     // Start is called before the first frame update
     void Start()
     {
         path = string.Format("{0}_markers.txt", DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-fff"));
+        logBuffer = new string[logBufferSize];
     }
 
     // Update is called once per frame
@@ -28,29 +30,44 @@ public class PrintMarkerLocations : MonoBehaviour
 
         List<OptitrackMarkerState> markerStates = streamingClient.GetLatestMarkerStates();
 
+        string timeStamp = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-fff");
         foreach (OptitrackMarkerState markerState in markerStates)
         {
             if (boundingBox.GetComponent<Collider>().bounds.Contains(markerState.Position))
             {
-                String log = string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n", DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-fff"),
+                String log = string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", timeStamp,
                     logIndex, markerState.Id, markerState.Position.x, markerState.Position.y, markerState.Position.z);
 
-                PrintTextToFile(log);
+                WriteToBuffer(log);
                 logIndex += 1;
             }
         }
     }
 
-    public void PrintTextToFile(string text)
+    public void WriteToBuffer(string text)
     {
-        line_ = text;
-
-        var thread = new Thread(WriteOnSeparateThread);
+        var thread = new Thread(() => WriteOnSeparateThread(logIndex, text));
         thread.Start();
     }
 
-    private void WriteOnSeparateThread()
+    private void WriteOnSeparateThread(int logIndex, string logMessage)
     {
-        File.AppendAllText(path, line_);
+        logBuffer[logIndex] = logMessage;
     }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("Application ended.");
+        ExportToFile();
+    }
+
+    private void ExportToFile()
+    {
+        using (TextWriter tw = new StreamWriter(path))
+        {
+            foreach (String s in logBuffer)
+                tw.WriteLine(s);
+        }
+    }
+
 }
