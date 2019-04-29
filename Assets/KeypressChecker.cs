98369cc7f61126c15538f4ad1602caede45d1a5c
dl541 <dl541@cam.ac.uk>
2019-04-29 19:22:59 +0100

    Add functions for transforming coordinate systems

--- Assets/KeypressChecker.cs
+++ Assets/KeypressChecker.cs
@@ -10,17 +10,29 @@ public class KeypressChecker : MonoBehaviour {
     private TFGraph rightNNGraph;
     private TFSession leftNNSession;
     private TFSession rightNNSession;
+    public GameObject topLeftMarker;
+    public GameObject topRightMarker;
+    public GameObject sideLeftMarker;
 
 	// Use this for initialization
 	void Start () {
         leftNNGraph = new TFGraph();
         leftNNGraph.Import(File.ReadAllBytes("Assets/tf_NN_focal_clf_left.pb"));
         leftNNSession = new TFSession(leftNNGraph);
-        
-        for (int i = 0; i<10; i++)
+
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        for (int i = 0; i<100; i++)
         {
-            Debug.Log(IsLeftKeypress(GetTestVector()));
+            //TestTransform();
+
+            var testVector = GetTestVector();
+            Debug.Log(IsLeftKeypress(testVector));
         }
+
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+
+        Debug.Log(string.Format("Time taken for 10 iterations: {0}", elapsedMs));
     }
 	
 	// Update is called once per frame
@@ -64,5 +76,50 @@ public class KeypressChecker : MonoBehaviour {
             0.00000f,    0.000000f,    0.000000f,   0.000000f,   0.000000f };
 
         return new float[][] { testVector };
+    }
+
+    private void TestTransform()
+    {
+        var testPos = new Vector3(-0.07221564f, 0.7727299f, 0.5003643f);
+
+        var testTransformMatrix = GetTestTransformMatrix();
+        Debug.Log(testTransformMatrix);
+        Debug.Log(testTransformMatrix.MultiplyPoint3x4(testPos).ToString("F7"));
+    }
+
+    private Matrix4x4 GetTestTransformMatrix()
+    {
+        var topLeftPos = new Vector3(-0.1130678f, 0.7825412f, 0.5232269f);
+        var topRightPos = new Vector3(5.866859e-04f, 7.746999e-01f, 5.192038e-01f);
+        var sideLeftPos = new Vector3(-0.1385423f, 0.7663757f, 0.5191758f);
+
+        return FindTransformMatrix(topLeftPos, topRightPos, sideLeftPos);
+    }
+
+    private Matrix4x4 FindTransformMatrix(Vector3 topLeftPos, Vector3 topRightPos, Vector3 sideLeftPos)
+    {
+        var invTransformMatrix = Matrix4x4.identity;
+
+        // x vector points from top left to top right
+        // y vector points out of the plane
+        // z vector points from top left to bottom left
+        Vector3 xColVector = (topRightPos - topLeftPos).normalized;
+        Vector3 yColVector = Vector3.Cross(xColVector, sideLeftPos - topLeftPos).normalized;
+        Vector3 zColVector = Vector3.Cross(xColVector, yColVector).normalized;
+
+        invTransformMatrix.SetColumn(0, xColVector);
+        invTransformMatrix.SetColumn(1, yColVector);
+        invTransformMatrix.SetColumn(2, zColVector);
+
+        Matrix4x4 transformMatrix = invTransformMatrix.inverse;
+        // Top left marker is the new origin
+        Vector3 translationVector = -topLeftPos;
+        translationVector = transformMatrix.MultiplyPoint3x4(translationVector);
+        Debug.Log(string.Format("translation:{0}",translationVector));
+        Vector4 translationVector4 = translationVector;
+        translationVector4.w = 1.0f;
+        transformMatrix.SetColumn(3, translationVector);
+
+        return transformMatrix;
     }
 }
